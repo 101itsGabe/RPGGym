204730c6676d3cf490a217a05622537bfd438a22
101itsGabe <105684932+101itsGabe@users.noreply.github.com>
2023-05-29 15:56:36 -0400

    First Commit

---
+++ GymRPG.Library/Models/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRPG.Library.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public string CharClass { get; set; }
+        public int Level;
+        public int CharacterId { get; set; }
+
+        public List<Workout> Workouts { get; set; }
+
+        public Character() 
+        {
+            Workouts = new List<Workout>();
+        }
+
+
+    }
+}
---
+++ GymRPG.Library/Models/Workout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRPG.Library.Models
+{
+    public class Workout
+    {
+        public string WorkoutName { get; set; }
+        public int WOLevel { get; set; }
+    }
+}
